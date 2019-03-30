be536220506b3fb8660157f28a0f8e8e6d217d64
Soleil Yu <shengyang998@foxmail.com>
2019-03-30 23:26:05 +0800

    update cocoapods and convert to swift 5.0

--- Kittygram/Networking/Endpoints/GitHub.swift
+++ Kittygram/Networking/Endpoints/GitHub.swift
@@ -24,6 +24,7 @@ enum GitHub {
 extension GitHub: TargetType {
     
     var baseURL: URL { return URL(string: "https://api.github.com")! }
+    
     var path: String {
         switch self {
         case .userRepositories(let name):
@@ -36,12 +37,23 @@ extension GitHub: TargetType {
             return "/repos/\(name)"
         }
     }
+    
     var method: Moya.Method {
         return .get
     }
+    
     var parameters: [String: Any]? {
         return nil
     }
+    
+    var headers: [String : String]? {
+        return [
+            "Accept": "application/json",
+            "Accept-Language": "",
+            "Content-Type": "application/json"
+        ]
+    }
+    
     var sampleData: Data {
         switch self {
         case .userRepositories(_):
@@ -54,7 +66,9 @@ extension GitHub: TargetType {
             return "{\"id\": \"1\", \"language\": \"Swift\", \"url\": \"https://api.github.com/repos/mjacko/Router\", \"name\": \"Router\"}".data(using: .utf8)!
         }
     }
+    
     var task: Task {
-        return .request
+        return .requestPlain
     }
+    
 }
